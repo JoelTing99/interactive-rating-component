410cd7421ba05f4baf3ead6c56ebe1397e5fad11
Joe <93369182+JoelTing99@users.noreply.github.com>
2023-04-19 18:11:16 +0800

    add javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+let ratingItems = document.querySelectorAll("li");
+let selectValue = 0;
+
+ratingItems.forEach(element => {
+    element.addEventListener("click", () => {
+        ratingItems.forEach(element => {
+            element.style.backgroundColor = "#262f36";
+            element.style.color = "	#656d77";
+        });
+        element.style.backgroundColor = "#fb7617";
+        element.style.color = "#fff";
+        
+        selectValue = element.value;
+    });
+    
+    element.addEventListener("mouseover", () => {
+        if (element.value !== selectValue)
+        {
+            element.style.backgroundColor = "#959eac";
+            element.style.color = "#fff";
+        }
+    });
+
+    element.addEventListener("mouseout", () => {
+        if (element.value !== selectValue)
+        {
+            element.style.backgroundColor = "#262f36";
+            element.style.color = "	#656d77";
+        }
+    });
+});
+
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    document.querySelector(".rating-card").classList.add("hidden");
+    document.querySelector(".thanks-card").classList.remove("hidden");
+});
